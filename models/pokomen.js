4e17b70e111ca3b1a9cd753da3dc752496acbdb8
Viljami Ruokonen <vruokonen@gmail.com>
2020-12-10 18:56:02 +0200

    added pokemon schema

---
+++ models/pokomen.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose'),
+    { Schema } = mongoose,
+
+pokemonSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        lowercase: true,
+        unique: true,
+    },
+    type: {
+        type: [String],
+        required: true,
+        lowercase: true,
+    },
+    base: {
+        hp:{
+            type: Number,
+            required: true,
+        },
+        attack:{
+            type: Number,
+            required: true
+        },
+        defence: {
+            type: Number,
+            required: true
+        },
+        sp_attack: {
+            type: Number
+        },
+        sp_defence: {
+            type: Number
+        },
+        speed: {
+            type: Number,
+        }
+    },
+},
+    { timestamps: true });
+
+module.exports = mongoose.model('Pokemon', pokemonSchema);
