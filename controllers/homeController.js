6bbed4cc739892ee063d04d5d14e433976a9020a
Viljami Ruokonen <vruokonen@gmail.com>
2020-12-11 23:27:58 +0200

    pokemon ejs shows pokemons n...x from q parameter

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,8 +1,18 @@
+const Pokemon = require('../models/pokemon')
+
 module.exports = {
     index: (req, res) => {
         res.render("index");
     },
     pokemon: (req, res) => {
-        res.render("pokemons");
+        let index_array = [];
+        let pokemons_from_to = req.query.pokemons;
+        pokemons_from_to = pokemons_from_to.split('-')
+        for(let i = pokemons_from_to[0]; i<= pokemons_from_to[1]; i++){
+            index_array.push(i);
+        }
+        Pokemon.find().where('pokemon_index').in(index_array).exec((err, records) => {
+            res.render("pokemons", {pokemons: records});
+        });
     }
 };
