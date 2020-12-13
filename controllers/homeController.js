ee874601a15bf110f8c115d319f8b99e9ed422de
Viljami Ruokonen <vruokonen@gmail.com>
2020-12-13 17:02:47 +0200

    next and previous buttons to pokemon page

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -30,7 +30,8 @@ module.exports = {
         let index_array = [];
         let pokemons_from_to = req.query.pokemons;
         pokemons_from_to = pokemons_from_to.split('-')
-        for(let i = pokemons_from_to[0]; i<= pokemons_from_to[1]; i++){
+        res.locals.from_to = pokemons_from_to;
+        for(let i = parseInt(pokemons_from_to[0]); i<= parseInt(pokemons_from_to[1]); i++){
             index_array.push(i);
         }
         Pokemon.find().where('pokemon_index').in(index_array).exec((err, records) => {
